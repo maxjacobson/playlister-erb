372e304cf7993028d77c7ea5d140b12bc5dc0cda
Max Jacobson <maxwell.jacobson@gmail.com>
2013-06-19 11:43:52 -0400

    not sure tbh

--- app.rb
+++ app.rb
@@ -15,20 +15,18 @@ class Jukebox
   end
 
   def parse_directory(dir_name)
-    songs = Dir.entries(dir_name).delete_if{|str| str == "." || str == ".."}
+    songs = Dir.entries(dir_name).delete_if{|str| str[0] == "."}
     songs.each do |filename|
       artist_name = filename.split(" - ")[0]
       song_name = filename.split(" - ")[1].split("[")[0].strip
       genre_name = filename.split(" - ")[1].split(/\[|\]/)[1]
 
-      artist = Artist.find_by_name(artist_name) || Artist.new # use tap here to assign name
-      artist.name = artist_name
+      artist = Artist.find_by_name(artist_name) || Artist.new.tap{|a| a.name = artist_name}
       
       song = Song.new
       song.name = song_name
       
-      genre = Genre.find_by_name(genre_name) || Genre.new # use tap here to assign name
-      genre.name = genre_name
+      genre = Genre.find_by_name(genre_name) || Genre.new.tap{|g| g.name = genre_name}
 
       song.genre = genre
       artist.add_song(song)
@@ -132,4 +130,5 @@ class Jukebox
     @on
   end
 end
+
 Jukebox.new("data")
